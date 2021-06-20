67116e902c69ce1a8760452804c17c3844d260db
nadia-blzrg <85636291+nadia-blzrg@users.noreply.github.com>
2021-06-20 18:54:11 +0200

    Update model.py

--- model.py
+++ model.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import pickle
 
-dataset = pd.read_csv('hiring.csv')
+dataset = pd.read_csv('dataset.csv')
 
 dataset['experience'].fillna(0, inplace=True)
 
