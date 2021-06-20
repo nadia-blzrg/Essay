50ce81aee0dfea9cb73ceac49c60c048c98bbce3
unknown <nadianbellazrag@gmail.com>
2021-06-20 18:24:11 +0200

    first commit

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
+import pickle
 import numpy as np
 from flask import Flask, request, jsonify, render_template
-import joblib
+import pickle
 
 app = Flask(__name__)
-model = joblib.load(open('model.pkl', 'rb'))
+model =pickle.load(open('model.pkl', 'rb'))
 
 @app.route('/')
 def home():
