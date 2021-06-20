bc1cf1957fa50691b7ca2ed0542aca159b8ebcd8
nadia-blzrg <85636291+nadia-blzrg@users.noreply.github.com>
2021-06-21 01:37:03 +0200

    Update model.py

--- model.py
+++ model.py
@@ -2,7 +2,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
-import pickle
+import joblib
 
 dataset = pd.read_csv('dataset.csv')
 
@@ -11,14 +11,6 @@ dataset['experience'].fillna(0, inplace=True)
 dataset['test_score'].fillna(dataset['test_score'].mean(), inplace=True)
 
 X = dataset.iloc[:, :3]
-
-#Converting words to integer values
-def convert_to_int(word):
-    word_dict = {'one':1, 'two':2, 'three':3, 'four':4, 'five':5, 'six':6, 'seven':7, 'eight':8,
-                'nine':9, 'ten':10, 'eleven':11, 'twelve':12, 'zero':0, 0: 0}
-    return word_dict[word]
-
-X['experience'] = X['experience'].apply(lambda x : convert_to_int(x))
 
 y = dataset.iloc[:, -1]
 
@@ -32,8 +24,8 @@ regressor = LinearRegression()
 regressor.fit(X, y)
 
 # Saving model to disk
-pickle.dump(regressor, open('model.pkl','wb'))
+joblib.dump(regressor, open('model.pkl','wb'))
 
 # Loading model to compare the results
-model = pickle.load(open('model.pkl','rb'))
+model = joblib.load(open('model.pkl','rb'))
 print(model.predict([[2, 9, 6]]))
